7365bb0ae64056129944d10251e8aa40ac4ad459
daveg-fwm <wddavegreen@gmail.com>
2019-01-25 14:52:50 +0200

    Moved header state and animations to Layout component

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { navigate } from 'gatsby';
-import anime from 'animejs';
 import { HeaderPanelStyled, NavButtonStyled } from './styles/HeaderPanelStyled';
 import { FWMIcon } from './svg/InlineSVG';
 import HeaderContent from './HeaderContent';
@@ -15,91 +14,25 @@ class Header extends React.Component {
       })
     ).isRequired,
     data: PropTypes.object.isRequired,
-  };
-
-  state = {
-    showMenu: false,
-    windowWidth: window.innerWidth,
-    windowHeight: window.innerHeight,
-    // Create a ref for the HeaderContent component - value passed up to this Header component via props
-    headerContentRef: React.createRef(),
-  };
-
-  // Create a ref for the nav element
-  navRef = React.createRef();
-
-  componentDidMount = () => {
-    const { headerContentRef } = this.state;
-
-    // Animate HeaderContent component in
-    anime({
-      targets: headerContentRef.current,
-      translateY: () => ['-100%', '0%'],
-      easing: 'easeOutCubic',
-      duration: 500,
-    });
-
-    window.addEventListener('resize', this.updateSize);
-  };
-
-  // Prevent memory leaks
-  componentWillUnmount = () => {
-    window.removeEventListener('resize', this.updateSize);
-  };
-
-  // Animation values vary depending on window size
-  updateSize = () => {
-    this.setState({
-      windowWidth: window.innerWidth,
-      windowHeight: window.innerHeight,
-    });
-  };
-
-  // Toggle showMenu state to open/close nav menu
-  toggleNav = () => {
-    const { showMenu } = this.state;
-
-    this.setState({ showMenu: !showMenu });
-
-    // Animate nav menu open/close
-    anime({
-      targets: this.navRef.current,
-      translateY: () => {
-        if (!showMenu) {
-          return ['-100%', '0%'];
-        }
-        // Menu is closed
-        return ['0%', '-100%'];
-      },
-      easing: 'easeOutCubic',
-      duration: 500,
-    });
-  };
-
-  hideContent = () => {
-    const { showMenu, headerContentRef } = this.state;
-    const delay = showMenu ? 500 : 0;
-
-    // Close nav menu if it's open
-    if (showMenu) this.toggleNav();
-
-    // Hide HeaderContent component
-    anime({
-      targets: headerContentRef.current,
-      translateY: () => ['0%', '-100%'],
-      easing: 'easeOutCubic',
-      duration: 500,
-      delay,
-    });
+    navRef: PropTypes.object.isRequired,
+    headerContentRef: PropTypes.object.isRequired,
+    toggleNav: PropTypes.func.isRequired,
+    hideHeaderContent: PropTypes.func.isRequired,
   };
 
   render() {
-    const { menuLinks, data } = this.props;
-    const { headerContentRef } = this.state;
+    const {
+      menuLinks,
+      data,
+      navRef,
+      headerContentRef,
+      toggleNav,
+      hideHeaderContent,
+    } = this.props;
 
     return (
       <HeaderPanelStyled>
-        <div className="header-panel-inner" ref={this.headerRef}>
+        <div className="header-panel-inner">
           <div className="top-nav">
             <a
               href="/"
@@ -109,14 +42,14 @@ class Header extends React.Component {
 
                 // Only call functions if link does not match current page
                 if (window.location.pathname !== '/') {
-                  this.hideContent();
+                  hideHeaderContent();
                   setTimeout(() => navigate('/'), 1000);
                 }
               }}
             >
               <FWMIcon />
             </a>
-            <NavButtonStyled type="button" onClick={this.toggleNav}>
+            <NavButtonStyled type="button" onClick={() => toggleNav()}>
               <span />
               <span />
               <span />
@@ -129,7 +62,7 @@ class Header extends React.Component {
             */}
             <HeaderContent data={data} headerContentRef={headerContentRef} />
 
-            <nav ref={this.navRef}>
+            <nav ref={navRef}>
               <ul>
                 {/* menuLinks contains page link data - received from Layout component */}
                 {menuLinks.map(item => (
@@ -144,7 +77,7 @@ class Header extends React.Component {
 
                         // Only call functions if link does not match current page
                         if (window.location.pathname !== item.link) {
-                          this.hideContent();
+                          hideHeaderContent();
                           setTimeout(() => navigate(`${item.link}`), 1000);
                         }
                       }}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
+import anime from 'animejs';
 import GlobalStyled from './styles/GlobalStyled';
 import Header from './header';
 import MainPanelStyled from './styles/MainPanelStyled';
@@ -13,12 +14,67 @@ class Layout extends React.Component {
     footerClass: PropTypes.object.isRequired,
   };
 
+  state = {
+    showMenu: false,
+    // Create a ref for the nav element - value passed up from Header component via props
+    navRef: React.createRef(),
+    // Create a ref for the HeaderContent component - value passed up from HeaderContent component via props
+    headerContentRef: React.createRef(),
+  };
+
   componentDidMount = () => {
-    // console.log(document.getElementById('rsc-project').offsetTop);
+    const { headerContentRef } = this.state;
+
+    // Animate HeaderContent component in
+    anime({
+      targets: headerContentRef.current,
+      translateY: () => ['-100%', '0%'],
+      easing: 'easeOutCubic',
+      duration: 500,
+    });
+  };
+
+  // Toggle showMenu state to open/close nav menu
+  toggleNav = () => {
+    const { showMenu, navRef } = this.state;
+
+    this.setState({ showMenu: !showMenu });
+
+    // Animate nav menu open/close
+    anime({
+      targets: navRef.current,
+      translateY: () => {
+        if (!showMenu) {
+          return ['-100%', '0%'];
+        }
+        // Menu is closed
+        return ['0%', '-100%'];
+      },
+      easing: 'easeOutCubic',
+      duration: 500,
+    });
+  };
+
+  hideHeaderContent = () => {
+    const { showMenu, headerContentRef } = this.state;
+    const delay = showMenu ? 500 : 0;
+
+    // Close nav menu if it's open
+    if (showMenu) this.toggleNav();
+
+    // Hide HeaderContent component
+    anime({
+      targets: headerContentRef.current,
+      translateY: () => ['0%', '-100%'],
+      easing: 'easeOutCubic',
+      duration: 500,
+      delay,
+    });
   };
 
   render() {
     const { children, headerData, footerClass } = this.props;
+    const { navRef, headerContentRef } = this.state;
 
     return (
       // Fetch page link data from gatsby-config.js (centralized navigation)
@@ -47,6 +103,10 @@ class Layout extends React.Component {
             <Header
               menuLinks={data.site.siteMetadata.menuLinks}
               data={headerData}
+              navRef={navRef}
+              headerContentRef={headerContentRef}
+              toggleNav={this.toggleNav}
+              hideHeaderContent={this.hideHeaderContent}
             />
 
             <MainPanelStyled>{children}</MainPanelStyled>
