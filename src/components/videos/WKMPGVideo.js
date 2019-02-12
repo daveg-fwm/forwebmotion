10f8b8f2aa9f4301aa19a8352d704b67da67c16a
daveg-fwm <wddavegreen@gmail.com>
2019-02-12 14:48:52 +0200

    Fixing Safari issues

--- src/components/videos/WKMPGVideo.js
+++ src/components/videos/WKMPGVideo.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types';
 import video from '../../videos/wkm-payment-gateway.mp4';
 
 const WKMPGVideo = ({ toggleVideo }) => (
-  <video id="wkm-gateway-video" loop muted onClick={() => toggleVideo('pause')}>
+  <video
+    id="wkm-gateway-video"
+    loop
+    playsinline
+    muted
+    onClick={() => toggleVideo('pause')}
+  >
     <source src={video} type="video/mp4" />
   </video>
 );
