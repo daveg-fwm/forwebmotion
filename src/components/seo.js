cbb99b5de8d3506b88a19af3a63cc846a151d153
daveg-fwm <wddavegreen@gmail.com>
2020-04-01 01:04:24 +0200

    Update robots seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, keywords, title }) {
             meta={[
               {
                 name: 'robots',
-                content: 'noindex, nofollow',
+                content: 'index, follow',
               },
               {
                 name: 'description',
